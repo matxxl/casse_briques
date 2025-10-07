54cd5a4db9cddce4446159cd3cab848644124649
matpiot <matheo.piot@cpe.fr>
2025-10-07 11:30:12 +0200

    update brique

--- brique.py
+++ brique.py
@@ -1,3 +1,17 @@
 class Brique :
-    def __breique__(self):
-        return True
+    
+    couleur_brique = "blue"
+    
+    def __init__(self, x, y, hits):
+        self.width = 75
+        self.height = 20
+        self.position = PVecteur(x, y)
+        self.hits = hits
+        self.couleur = Brique.couleur_brique
+
+    def display(self):
+        fill(self.couleur)
+        stroke("black")
+        strokeWeight(2)
+        rect(self.position.x, self.position.y, self.width, self.height)
+        
